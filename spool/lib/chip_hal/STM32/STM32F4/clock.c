6cf95967b38649d43ebe643fa4cd7773dfb249c1
Dummyc0m <xieyuanchu@gmail.com>
2023-01-17 23:47:15 -0800

    configure clock, interface to be improved

--- spool/lib/chip_hal/STM32/STM32F4/clock.c
+++ spool/lib/chip_hal/STM32/STM32F4/clock.c
@@ -7,72 +7,56 @@
 static void configurePllCfgr(void)
 {
   /* PLL values M25 N336 P4 Q7 */
-  uint32_t pll_cfgr = REG_RD32(DRF_RCC_PLLCFGR);
+  uint32_t pll_cfgr = REG_RD32(DRF_REG(_RCC, _PLLCFGR));
 
   /* PLL Q = 7 0111 */
-  pll_cfgr = FLD_SET_DRF(_RCC, _PLLCFGR, _PLLQ3, _CLR, pll_cfgr);
-  pll_cfgr = FLD_SET_DRF(_RCC, _PLLCFGR, _PLLQ2, _SET, pll_cfgr);
-  pll_cfgr = FLD_SET_DRF(_RCC, _PLLCFGR, _PLLQ1, _SET, pll_cfgr);
-  pll_cfgr = FLD_SET_DRF(_RCC, _PLLCFGR, _PLLQ0, _SET, pll_cfgr);
+  pll_cfgr = FLD_SET_DRF_NUM(_RCC, _PLLCFGR, _PLLQ, 7, pll_cfgr);
 
   /* PLL SRC = HSE */
-  pll_cfgr = FLD_SET_DRF(_RCC, _PLLCFGR, _PLLSRC, _SET, pll_cfgr);
+  pll_cfgr = FLD_SET_DRF(_RCC, _PLLCFGR, _PLLSRC, _HSE, pll_cfgr);
 
   /* PLL P = 4 01 */
-  pll_cfgr = FLD_SET_DRF(_RCC, _PLLCFGR, _PLLP1, _CLR, pll_cfgr);
-  pll_cfgr = FLD_SET_DRF(_RCC, _PLLCFGR, _PLLP0, _SET, pll_cfgr);
+  pll_cfgr = FLD_SET_DRF(_RCC, _PLLCFGR, _PLLP, _DIV4, pll_cfgr);
 
   /* PLL N = 336 101010000 */
-  pll_cfgr = FLD_SET_DRF(_RCC, _PLLCFGR, _PLLN8, _SET, pll_cfgr);
-  pll_cfgr = FLD_SET_DRF(_RCC, _PLLCFGR, _PLLN7, _CLR, pll_cfgr);
-  pll_cfgr = FLD_SET_DRF(_RCC, _PLLCFGR, _PLLN6, _SET, pll_cfgr);
-  pll_cfgr = FLD_SET_DRF(_RCC, _PLLCFGR, _PLLN5, _CLR, pll_cfgr);
-  pll_cfgr = FLD_SET_DRF(_RCC, _PLLCFGR, _PLLN4, _SET, pll_cfgr);
-  pll_cfgr = FLD_SET_DRF(_RCC, _PLLCFGR, _PLLN3, _CLR, pll_cfgr);
-  pll_cfgr = FLD_SET_DRF(_RCC, _PLLCFGR, _PLLN2, _CLR, pll_cfgr);
-  pll_cfgr = FLD_SET_DRF(_RCC, _PLLCFGR, _PLLN1, _CLR, pll_cfgr);
-  pll_cfgr = FLD_SET_DRF(_RCC, _PLLCFGR, _PLLN0, _CLR, pll_cfgr);
+  pll_cfgr = FLD_SET_DRF_NUM(_RCC, _PLLCFGR, _PLLN, 336, pll_cfgr);
 
   /* PLL M = 25 011001 */
-  pll_cfgr = FLD_SET_DRF(_RCC, _PLLCFGR, _PLLM5, _CLR, pll_cfgr);
-  pll_cfgr = FLD_SET_DRF(_RCC, _PLLCFGR, _PLLM4, _SET, pll_cfgr);
-  pll_cfgr = FLD_SET_DRF(_RCC, _PLLCFGR, _PLLM3, _SET, pll_cfgr);
-  pll_cfgr = FLD_SET_DRF(_RCC, _PLLCFGR, _PLLM2, _CLR, pll_cfgr);
-  pll_cfgr = FLD_SET_DRF(_RCC, _PLLCFGR, _PLLM1, _CLR, pll_cfgr);
-  pll_cfgr = FLD_SET_DRF(_RCC, _PLLCFGR, _PLLM0, _SET, pll_cfgr);
+  pll_cfgr = FLD_SET_DRF_NUM(_RCC, _PLLCFGR, _PLLM, 25, pll_cfgr);
 
-  REG_WR32(DRF_RCC_PLLCFGR, pll_cfgr);
+  REG_WR32(DRF_REG(_RCC, _PLLCFGR), pll_cfgr);
 }
 
 void configureCr(void)
 {
-  /* wait for PLLRDY*/
-  while (FLD_TEST_DRF(_RCC, _CR, _PLLRDY, _CLR, REG_RD32(DRF_RCC_CR)))
+  uint32_t cr = REG_RD32(DRF_REG(_RCC, _CR));
+
+  cr = FLD_SET_DRF(_RCC, _CR, _PLLON, _ON, cr);
+  cr = FLD_SET_DRF(_RCC, _CR, _HSEON, _ON, cr);
+
+  REG_WR32(DRF_REG(_RCC, _CR), cr);
+
+  while (FLD_TEST_DRF(_RCC, _CR, _PLLRDY, _NOT_READY, REG_RD32(DRF_REG(_RCC, _CR))))
     ;
-  while (FLD_TEST_DRF(_RCC, _CR, _HSERDY, _CLR, REG_RD32(DRF_RCC_CR)))
+  while (FLD_TEST_DRF(_RCC, _CR, _HSERDY, _NOT_READY, REG_RD32(DRF_REG(_RCC, _CR))))
     ;
-
-  uint32_t cr = REG_RD32(DRF_RCC_CR);
-
-  cr = FLD_SET_DRF(_RCC, _CR, _PLLON, _SET, cr);
-  cr = FLD_SET_DRF(_RCC, _CR, _HSEON, _SET, cr);
-
-  REG_WR32(DRF_RCC_CR, cr);
 }
 
 void configureCfgr(void)
 {
-    uint32_t cfgr = REG_RD32(DRF_RCC_CFGR);
+    uint32_t cfgr = REG_RD32(DRF_REG(_RCC, _CFGR));
 
     /* divide by 2*/
-    cfgr = FLD_SET_DRF_NUM(_RCC, _CFGR, _PPRE1, 0b100, cfgr);
+    cfgr = FLD_SET_DRF(_RCC, _CFGR, _PPRE1, _DIV2, cfgr);
 
-    REG_WR32(DRF_RCC_CFGR, cfgr);
+    REG_WR32(DRF_REG(_RCC, _CFGR), cfgr);
 }
 
 void chipHalClockInit()
 {
     configurePllCfgr();
+
+    configureCfgr();
 
     configureCr();
 }
--- spool/spool.c
+++ spool/spool.c
@@ -4,13 +4,13 @@
 
 #include "chip_hal/hal.h"
 #include "chip_hal/stm32f401/gpio.h"
-#include "spool/lib/chip_hal/inc/clock.h"
 #include "spool_config.h"
 #include <stdint.h>
 
-
 void main()
 {
+    chipHalClockInit();
+
     REG_WR32(DRF_REG(_RCC,_AHB1ENR), FLD_SET_DRF(_RCC,_AHB1ENR,_GPIOCEN, _ENABLED, REG_RD32(DRF_REG(_RCC,_AHB1ENR))));
 
     struct IOLine led = chipHalGpioLineConstruct(GPIOC, 13);
