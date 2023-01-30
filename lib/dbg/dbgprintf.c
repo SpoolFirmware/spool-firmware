66af424eb7f7818886c51be43d74197a4575b020
codetector <codetector@codetector.org>
2023-01-29 23:59:48 -0800

    dbgPrintf use basePri raise

--- lib/dbg/dbgprintf.c
+++ lib/dbg/dbgprintf.c
@@ -10,31 +10,31 @@ static size_t head = 0, tail = 0;
 
 void dbgPutc(const char c)
 {
-    __asm__("cpsid i");
+    uint32_t basePri = ulPortRaiseBASEPRI();
     if ((tail + 1) % BUFFER_SIZE != head) {
         print_buffer[tail] = c;
-        __asm__("dsb");
+        __asm volatile("dsb");
         tail = (tail + 1) % BUFFER_SIZE;
     }
-    __asm__("cpsie i");
+    vPortSetBASEPRI(basePri);
 }
 
 void dbgPuts(const char *c)
 {
-    __asm__("cpsid i");
+    uint32_t basePri = ulPortRaiseBASEPRI();
     while((*c) != '\0' && (tail + 1) % BUFFER_SIZE != head) {
         print_buffer[tail] = *c++;
-        __asm__("dsb");
+        __asm__ volatile("dsb");
         tail = (tail + 1) % BUFFER_SIZE;
     }
-    __asm__("cpsie i");
+    vPortSetBASEPRI(basePri);
 }
 
 int dbgGetc(void)
 {
     if (head != tail) {
         char c = print_buffer[head];
-        __asm__("dsb");
+        __asm volatile("dsb");
         head = (head + 1) % BUFFER_SIZE;
         return c;
     }
