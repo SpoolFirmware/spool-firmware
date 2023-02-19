a5e6f1d93e4c6e76b12800aa7fae895a6ad27974
codetector <codetector@codetector.org>
2023-02-19 15:22:40 -0800

    move __warn to error.c

--- lib/common/error.c
+++ lib/common/error.c
@@ -12,4 +12,14 @@ _Noreturn void __panic(const char *file, int line, const char* err)
     for (volatile int i = line;; i = line);
 }
 
+void __warn(const char *file, int line, const char *err)
+{
+    dbgPrintf("WARN %s in %s:%d\n", err, file, line);
+}
+
+void __warn_on_err(const char *file, int line, status_t err)
+{
+    dbgPrintf("WARN ERR %d in %s:%d\n", err, file, line);
+}
+
 #pragma GCC diagnostic pop
