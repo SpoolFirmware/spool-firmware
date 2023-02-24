926f12234c34d2386e1f65214744ac3ba289b289
codetector <codetector@codetector.org>
2023-02-23 23:11:48 -0800

    [WIP] SPI

--- lib/hal/stm32/peripheral/spi_v1/spi.c
+++ lib/hal/stm32/peripheral/spi_v1/spi.c
@@ -1,7 +1,26 @@
 #include "spi.h"
 
-void halSpiInit(struct SPIDevice *pDevice, const struct SPIConfig *pConfig);
-void halSpiStart(struct SPIDevice *pDevice);
-void halSpiSend(struct SPIDevice *pDevice, const void* data, uint32_t len);
-void halSpiXchg(struct SPIDevice *pDevice, const void* pDataOut, void* pDataIn, uint32_t len);
-void halSpiWaitTxIdle(struct SPIDevice *pDevice);
+void halSpiInit(struct SPIDevice *pDevice, const struct SPIConfig *pConfig)
+{
+
+}
+
+void halSpiStart(struct SPIDevice *pDevice)
+{
+
+}
+
+void halSpiSend(struct SPIDevice *pDevice, const void* data, uint32_t len)
+{
+
+}
+
+void halSpiXchg(struct SPIDevice *pDevice, const void* pDataOut, void* pDataIn, uint32_t len)
+{
+
+}
+
+void halSpiWaitTxIdle(struct SPIDevice *pDevice)
+{
+
+}
