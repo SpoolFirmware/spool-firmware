68439b33a2eb8eb6c8b844ae57816ac2c3f0c4a2
codetector <codetector@codetector.org>
2023-03-10 18:45:18 -0800

    test panic and print

--- spool/lib/planner/lib.rs
+++ spool/lib/planner/lib.rs
@@ -1,21 +1,51 @@
 #![no_std]
+
 use core::{panic::PanicInfo, ptr::null};
 
 extern "C" {
     fn __panic(file: *const u8, line: i32, msg: *const u8) -> !;
+    fn dbgPutc(c: u8);
+
+}
+
+pub struct Console;
+impl core::fmt::Write for Console {
+    fn write_str(&mut self, s: &str) -> core::fmt::Result {
+        for s in s.bytes() {
+            unsafe { dbgPutc(s); }
+        }
+        Ok(())
+    }
+}
+static mut CONSOLE: Console = Console;
+
+pub fn dbgprint(args: core::fmt::Arguments) {
+    use core::fmt::Write;
+    unsafe {
+        let _ = CONSOLE.write_fmt(args);
+    };
+}
+
+macro_rules! println {
+    ($fmt:expr) => (print!(concat!($fmt, "\n")));
+    ($fmt:expr, $($arg:tt)*) => (print!(concat!($fmt, "\n"), $($arg)*));
+}
+
+macro_rules! print {
+    ($($arg:tt)*) => ({
+        $crate::dbgprint(format_args!($($arg)*));
+    });
 }
 
 #[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
+fn panic(_info: &PanicInfo) -> ! {
     unsafe {
-        match info.location() {
-            Some(loc) => __panic(loc.file().as_bytes().as_ptr(), loc.line() as i32, null()),
-            None => __panic(null(), 0, null()),
-        }
+        __panic(null(), 0, null());
     }
 }
 
 #[no_mangle]
 pub extern "C" fn plannerGetOne() -> u32 {
-    1
+    println!("hello");
+    panic!();
 }
